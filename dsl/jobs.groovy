e786710e3ba42d1e46b4cdce329a528c1a868b20
opeomotayo <43703454+opeomotayo@users.noreply.github.com>
2023-02-23 07:06:33 +0000

    testing sonar

--- dsl/jobs.groovy
+++ dsl/jobs.groovy
@@ -1,64 +1,87 @@
 // github('opeomotayo/cd-projects') can also be used instead of url('https://github.com/opeomotayo/cd-projects.git')
 // multiple pipelinesJobs can also be set here
 pipelineJob('flask-app1-build-image') {
-    logRotator {
-        numToKeep(10)
-        daysToKeep(30)
-    }
-    triggers {
-        githubPush()
-    }
-    definition {
-        cpsScm {
-        scm {
-            git {
-            remote {
-                github('opeomotayo/ci-projects')
-            }
-            branches('main')
-            }
-        }   
+  logRotator {
+    numToKeep(10)
+    daysToKeep(30)
+  }
+  triggers {
+    githubPush()
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            github('opeomotayo/ci-projects')
+          }
+          branches('main')
+        }
+      }   
         scriptPath('dsl/pipelines/flask-app1/Jenkinsfile')
-        }  
-    }       
+      }  
+  }       
 }
 
 pipelineJob('flask-app1-update-tag') {
-    logRotator {
-        numToKeep(10)
-        daysToKeep(30)
-    }
-    definition {
-        cpsScm {
-        scm {
-            git {
-            remote {
-                github('opeomotayo/cd-projects')
-            }
-            branches('main')
-            }
-        }   
-        scriptPath('technologies/deployment-pipelines/Jenkinsfile')
-        }  
-    }       
+  logRotator {
+    numToKeep(10)
+    daysToKeep(30)
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+              github('opeomotayo/cd-projects')
+          }
+          branches('main')
+        }
+      }   
+      scriptPath('technologies/deployment-pipelines/Jenkinsfile')
+    }  
+  }       
+}
+
+pipelineJob('java-app1-build-image') {
+  logRotator {
+    numToKeep(10)
+    daysToKeep(30)
+  }
+  triggers {
+    githubPush()
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            github('opeomotayo/ci-projects')
+          }
+          branches('main')
+        }
+      }   
+      scriptPath('dsl/pipelines/java-app1/Jenkinsfile')
+    }  
+  }       
 }
 
 pipelineJob('actions-controller') {
-    logRotator {
-        numToKeep(10)
-        daysToKeep(30)
-    }
-    definition {
-        cpsScm {
-        scm {
-            git {
-            remote {
-                github('opeomotayo/ci-projects')
-            }
-            branches('main')
-            }
-        }   
-        scriptPath('dsl/pipelines/actions-controller/Jenkinsfile')
-        }  
-    }       
+  logRotator {
+    numToKeep(10)
+    daysToKeep(30)
+  }
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            github('opeomotayo/ci-projects')
+          }
+          branches('main')
+        }
+      }   
+      scriptPath('dsl/pipelines/actions-controller/Jenkinsfile')
+    }  
+  }       
 }
