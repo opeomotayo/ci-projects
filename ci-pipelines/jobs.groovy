8486507d38c25059a1c7d69c030f52385f979e04
Ope Omotayo <43703454+opeomotayo@users.noreply.github.com>
2023-05-27 08:14:36 +0100

    testing a pipeline

--- ci-pipelines/jobs.groovy
+++ ci-pipelines/jobs.groovy
@@ -1,150 +1,150 @@
 // github('opeomotayo/cd-projects') can also be used instead of url('https://github.com/opeomotayo/cd-projects.git')
 // multiple pipelinesJobs can also be set here
-pipelineJob('actions-controller') {
-  logRotator {
-    numToKeep(10)
-    daysToKeep(30)
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            github('opeomotayo/ci-projects')
-          }
-          branches('main')
-        }
-      }   
-      scriptPath('ci-pipelines/actions-controller/Jenkinsfile')
-    }  
-  }       
-}
+// pipelineJob('actions-controller') {
+//   logRotator {
+//     numToKeep(10)
+//     daysToKeep(30)
+//   }
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote {
+//             github('opeomotayo/ci-projects')
+//           }
+//           branches('main')
+//         }
+//       }   
+//       scriptPath('ci-pipelines/actions-controller/Jenkinsfile')
+//     }  
+//   }       
+// }
 
-pipelineJob('todo-ci') {
-  logRotator {
-    numToKeep(10)
-    daysToKeep(30)
-  }
-  triggers {
-    githubPush()
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            github('opeomotayo/ci-projects')
-          }
-          branches('main')
-        }
-      }   
-      scriptPath('ci-pipelines/todo-ci/Jenkinsfile')
-    }  
-  }       
-}
-pipelineJob('todo-cd') {
-  logRotator {
-    numToKeep(10)
-    daysToKeep(30)
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-              github('opeomotayo/cd-projects')
-          }
-          branches('main')
-        }
-      }   
-      scriptPath('cd-pipelines/todo-cd/Jenkinsfile')
-    }  
-  }       
-}
+// pipelineJob('todo-ci') {
+//   logRotator {
+//     numToKeep(10)
+//     daysToKeep(30)
+//   }
+//   triggers {
+//     githubPush()
+//   }
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote {
+//             github('opeomotayo/ci-projects')
+//           }
+//           branches('main')
+//         }
+//       }   
+//       scriptPath('ci-pipelines/todo-ci/Jenkinsfile')
+//     }  
+//   }       
+// }
+// pipelineJob('todo-cd') {
+//   logRotator {
+//     numToKeep(10)
+//     daysToKeep(30)
+//   }
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote {
+//               github('opeomotayo/cd-projects')
+//           }
+//           branches('main')
+//         }
+//       }   
+//       scriptPath('cd-pipelines/todo-cd/Jenkinsfile')
+//     }  
+//   }       
+// }
 
-pipelineJob('flask-blog-ci') {
-  logRotator {
-    numToKeep(10)
-    daysToKeep(30)
-  }
-  triggers {
-    githubPush()
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            github('opeomotayo/ci-projects')
-          }
-          branches('main')
-        }
-      }   
-      scriptPath('ci-pipelines/flask-blog-ci/Jenkinsfile')
-    }  
-  }       
-}
-pipelineJob('flask-blog-cd') {
-  logRotator {
-    numToKeep(10)
-    daysToKeep(30)
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-              github('opeomotayo/cd-projects')
-          }
-          branches('main')
-        }
-      }   
-      scriptPath('cd-pipelines/flask-blog-cd/Jenkinsfile')
-    }  
-  }       
-}
+// pipelineJob('flask-blog-ci') {
+//   logRotator {
+//     numToKeep(10)
+//     daysToKeep(30)
+//   }
+//   triggers {
+//     githubPush()
+//   }
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote {
+//             github('opeomotayo/ci-projects')
+//           }
+//           branches('main')
+//         }
+//       }   
+//       scriptPath('ci-pipelines/flask-blog-ci/Jenkinsfile')
+//     }  
+//   }       
+// }
+// pipelineJob('flask-blog-cd') {
+//   logRotator {
+//     numToKeep(10)
+//     daysToKeep(30)
+//   }
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote {
+//               github('opeomotayo/cd-projects')
+//           }
+//           branches('main')
+//         }
+//       }   
+//       scriptPath('cd-pipelines/flask-blog-cd/Jenkinsfile')
+//     }  
+//   }       
+// }
 
-pipelineJob('html-blog-ci') {
-  logRotator {
-    numToKeep(10)
-    daysToKeep(30)
-  }
-  triggers {
-    githubPush()
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-            github('opeomotayo/ci-projects')
-          }
-          branches('main')
-        }
-      }   
-      scriptPath('ci-pipelines/html-blog-ci/Jenkinsfile')
-    }  
-  }       
-}
-pipelineJob('html-blog-cd') {
-  logRotator {
-    numToKeep(10)
-    daysToKeep(30)
-  }
-  definition {
-    cpsScm {
-      scm {
-        git {
-          remote {
-              github('opeomotayo/cd-projects')
-          }
-          branches('main')
-        }
-      }   
-      scriptPath('cd-pipelines/html-blog-cd/Jenkinsfile')
-    }  
-  }       
-}
+// pipelineJob('html-blog-ci') {
+//   logRotator {
+//     numToKeep(10)
+//     daysToKeep(30)
+//   }
+//   triggers {
+//     githubPush()
+//   }
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote {
+//             github('opeomotayo/ci-projects')
+//           }
+//           branches('main')
+//         }
+//       }   
+//       scriptPath('ci-pipelines/html-blog-ci/Jenkinsfile')
+//     }  
+//   }       
+// }
+// pipelineJob('html-blog-cd') {
+//   logRotator {
+//     numToKeep(10)
+//     daysToKeep(30)
+//   }
+//   definition {
+//     cpsScm {
+//       scm {
+//         git {
+//           remote {
+//               github('opeomotayo/cd-projects')
+//           }
+//           branches('main')
+//         }
+//       }   
+//       scriptPath('cd-pipelines/html-blog-cd/Jenkinsfile')
+//     }  
+//   }       
+// }
 
 pipelineJob('bookstore-ci') {
   logRotator {
